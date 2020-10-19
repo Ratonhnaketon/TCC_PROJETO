11d6056c62efb073ed721f7a468e50a36b1740a0
Diego Boccoli Gallego <diego@gallego.com.br>
2020-10-19 15:00:36 -0300

    ref(ARObject): Updating ARObject

--- Assets/Scripts/ARObject.cs
+++ Assets/Scripts/ARObject.cs
@@ -9,13 +9,15 @@ public class ARObject
     public string name;
     public string description;
     public string id;
+    public float scale;
     public string[] hashtags;
 
-    public ARObject(string name, string description, string id, string[] hashtags)
+    public ARObject(string name, string description, string id, string[] hashtags, float scale)
     {
         this.name = name;
         this.description = description;
         this.id = id;
         this.hashtags = hashtags;
+        this.scale = scale;
     }
 }
